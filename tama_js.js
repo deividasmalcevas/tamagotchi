78fc3ae3c5b6ccc78d64f10da9f7664444af2184
deividasmalcevas <deividasmalcevas@gmail.com>
2024-05-20 10:15:52 +0300

    hunger anf fun and whack a mole update

--- tama_js.js
+++ tama_js.js
@@ -5,7 +5,15 @@ const select_tama = document.getElementById(`select_tama`);
 const hp_bar = document.getElementById('hp-fill');
 const hunger_bar = document.getElementById('hunger-fill');
 const fun_bar = document.getElementById('fun-fill');
+const lvl_bar = document.getElementById('lvl-fill');
+const end_wrapper = document.getElementById('end');
 const play_area = document.getElementById('play_area');
+const lvl_display = document.getElementById('tama_lvl');
+const btn_feed = document.getElementById('btn_feed');
+const btn_play = document.getElementById('btn_play');
+const holes = document.querySelectorAll('.hole');
+const scoreBoard = document.getElementById('score');
+const game_start = document.getElementById('game_start');
 const Tamas = [
     { name: 'ant', emoji: '🐜' },
     { name: 'badger', emoji: '🦡' },
@@ -94,9 +102,85 @@ const Tamas = [
 ];
 let Selected_Tama;
 let move = true;
+let tama_hp;
+let tama_hunger;
+let tama_fun;
+let tama_exp;
+let tama_lvl;
+let size = 20;
+let lastHole;
+let timeUp = false;
+let score = 0;
 //init
 select_board();
+//btn click
+if (btn_feed) {
+    btn_feed.onclick = () => {
+        if (tama_hunger < 100) {
+            tama_hunger++;
+            update_bars(tama_hp, tama_hunger, tama_fun, tama_exp);
+        }
+    };
+}
+if (btn_play) {
+    btn_play.onclick = () => {
+        game_start?.classList.toggle(`hidden`);
+        startGame();
+    };
+}
 //functions
+//whack mole
+function randomHole(holes) {
+    const index = Math.floor(Math.random() * holes.length);
+    const hole = holes[index];
+    if (hole === lastHole) {
+        return randomHole(holes);
+    }
+    lastHole = hole;
+    return hole;
+}
+function peep() {
+    const time = RNG(200, 1000);
+    const hole = randomHole(holes);
+    hole.textContent = Selected_Tama.emoji;
+    hole.classList.add('up');
+    setTimeout(() => {
+        hole.textContent = ``;
+        hole.classList.remove('up');
+        if (!timeUp)
+            peep();
+    }, time);
+}
+function startGame() {
+    scoreBoard.textContent = '0';
+    timeUp = false;
+    score = 0;
+    peep();
+    setTimeout(() => {
+        timeUp = true;
+        tama_fun += score;
+        game_start?.classList.toggle(`hidden`);
+        update_bars(tama_hp, tama_hunger, tama_fun, tama_exp);
+    }, 10000);
+}
+function whack(event) {
+    if (!(event.target instanceof HTMLElement))
+        return;
+    const target = event.target;
+    const hole = target.closest('.hole');
+    if (!event.isTrusted || !hole || !hole.classList.contains('up'))
+        return;
+    score++;
+    scoreBoard.textContent = score.toString();
+    hole.classList.remove('up');
+}
+holes.forEach(hole => {
+    const mole = document.createElement('div');
+    mole.classList.add('mole');
+    hole.appendChild(mole);
+    hole.addEventListener('click', whack);
+});
+//
 function select_board() {
     if (!select_tama)
         return console.log(`empty tama List`);
@@ -110,7 +194,12 @@ function select_board() {
                 return console.log(`empty start div`);
             start.classList.toggle(`hidden`);
             Selected_Tama = new_tama;
-            update_bars(RNG(60, 80), RNG(40, 70), RNG(40, 70));
+            tama_hp = RNG(60, 80);
+            tama_hunger = RNG(40, 70);
+            tama_fun = RNG(40, 70);
+            tama_exp = 0;
+            tama_lvl = 0;
+            update_bars(tama_hp, tama_hunger, tama_fun, tama_exp);
             add_tama_to_playground(Selected_Tama);
         };
         select_tama.appendChild(tama);
@@ -120,28 +209,71 @@ function add_tama_to_playground(new_tama) {
     if (play_area) {
         const newTama = document.createElement('div');
         newTama.classList.add('new_tama');
+        newTama.style.fontSize = `${size}px`;
         newTama.textContent = new_tama.emoji;
         play_area.appendChild(newTama);
-        move_anim(newTama, play_area.clientWidth - newTama.clientWidth, play_area.clientHeight - newTama.clientHeight);
+        move_anim(newTama);
     }
 }
-function move_anim(newTama, maxX, maxY) {
-    const duration = RNG(2000, 5000);
-    const delay = RNG(0, 1000);
-    newTama.style.transition = `left ${duration}ms ease-out ${delay}ms, top ${duration}ms ease-out ${delay}ms`;
-    newTama.style.left = `${RNG(0, maxX)}px`;
-    newTama.style.top = `${RNG(0, maxY)}px`;
-    setTimeout(() => {
-        move_anim(newTama, maxX, maxY);
-    }, duration + delay);
+function move_anim(newTama) {
+    let maxX;
+    let maxY;
+    if (play_area) {
+        maxX = play_area.clientWidth - newTama.clientWidth;
+        maxY = play_area.clientHeight - newTama.clientHeight;
+        const duration = RNG(2000, 5000);
+        const delay = RNG(0, 1000);
+        newTama.style.transition = `left ${duration}ms ease-out ${delay}ms, top ${duration}ms ease-out ${delay}ms`;
+        newTama.style.left = `${RNG(0, maxX)}px`;
+        newTama.style.top = `${RNG(0, maxY)}px`;
+        setTimeout(() => {
+            if (move) {
+                check_status(newTama);
+                update_bars(tama_hp, tama_hunger, tama_fun, tama_exp);
+                move_anim(newTama);
+            }
+        }, duration + delay);
+    }
 }
-function update_bars(hp, hunger, fun) {
+function check_status(newTama) {
+    if (tama_hp > 0) {
+        if (tama_fun > 0 && tama_hunger > 0) {
+            if (tama_lvl < 100) {
+                tama_hunger--;
+                tama_fun--;
+                tama_exp += (100 - tama_lvl) / 100;
+                if (tama_exp >= 100) {
+                    tama_exp = 0;
+                    lvl_up(newTama);
+                }
+            }
+        }
+        else {
+            tama_hp -= (100 - tama_lvl) / 100;
+        }
+    }
+    else {
+        move = false;
+        newTama.remove();
+        end_wrapper?.classList.toggle(`hidden`);
+    }
+}
+function lvl_up(newTama) {
+    tama_lvl++;
+    size += 5;
+    newTama.style.fontSize = `${size}px`;
+    if (lvl_display)
+        lvl_display.textContent = tama_lvl.toString();
+}
+function update_bars(hp, hunger, fun, lvl) {
     if (hp_bar)
         setProgress(hp_bar, hp);
     if (hunger_bar)
         setProgress(hunger_bar, hunger);
     if (fun_bar)
         setProgress(fun_bar, fun);
+    if (lvl_bar)
+        setProgress(lvl_bar, lvl);
 }
 function setProgress(element, value) {
     element.style.width = value + '%';
--- tama_js.ts
+++ tama_js.ts
@@ -4,7 +4,16 @@ const select_tama = document.getElementById(`select_tama`) as HTMLDivElement | n
 const hp_bar = document.getElementById('hp-fill') as HTMLDivElement | null;
 const hunger_bar = document.getElementById('hunger-fill') as HTMLDivElement | null;
 const fun_bar = document.getElementById('fun-fill') as HTMLDivElement | null;
+const lvl_bar = document.getElementById('lvl-fill') as HTMLDivElement | null;
+const end_wrapper = document.getElementById('end') as HTMLDivElement | null;
 const play_area = document.getElementById('play_area') as HTMLDivElement | null;
+const lvl_display = document.getElementById('tama_lvl') as HTMLDivElement | null;
+const btn_feed = document.getElementById('btn_feed') as HTMLDivElement | null;
+const btn_play = document.getElementById('btn_play') as HTMLDivElement | null;
+const holes = document.querySelectorAll('.hole') as NodeListOf<HTMLElement>;
+const scoreBoard = document.getElementById('score') as HTMLSpanElement;
+const game_start = document.getElementById('game_start') as HTMLDivElement | null;
+
 //vars
 interface Tama_emoji {
     name: string;
@@ -98,10 +107,84 @@ const Tamas: Tama_emoji[] = [
 ];
 let Selected_Tama: Tama_emoji;
 let move: boolean = true
+let tama_hp: number
+let tama_hunger:number
+let tama_fun:number
+let tama_exp:number
+let tama_lvl:number
+let size:number = 20
+let lastHole: HTMLElement;
+let timeUp:boolean = false;
+let score:number = 0;
+
 //init
 select_board()
+//btn click
+if (btn_feed) {
+    btn_feed.onclick = () => {
+        if(tama_hunger < 100){
+            tama_hunger++
+            update_bars(tama_hp,tama_hunger,tama_fun,tama_exp)
+        }
+    };
+}
+if (btn_play) {
+    btn_play.onclick = () => {
+        game_start?.classList.toggle(`hidden`)
+        startGame()
+    }
+}
 
 //functions
+//whack mole
+function randomHole(holes: NodeListOf<HTMLElement>): HTMLElement {
+    const index = Math.floor(Math.random() * holes.length);
+    const hole = holes[index];
+    if (hole === lastHole) {
+        return randomHole(holes);
+    }
+    lastHole = hole;
+    return hole;
+}
+function peep(): void {
+    const time = RNG(200, 1000);
+    const hole = randomHole(holes);
+    hole.textContent = Selected_Tama.emoji
+    hole.classList.add('up');
+    setTimeout(() => {
+        hole.textContent = ``
+        hole.classList.remove('up');
+        if (!timeUp) peep();
+    }, time);
+}
+function startGame(): void {
+    scoreBoard.textContent = '0';
+    timeUp = false;
+    score = 0;
+    peep();
+    setTimeout(() => {
+        timeUp = true
+        tama_fun += score
+        game_start?.classList.toggle(`hidden`)
+        update_bars(tama_hp,tama_hunger,tama_fun,tama_exp)
+    }, 10000);
+}
+function whack(event: Event): void {
+    if (!(event.target instanceof HTMLElement)) return;
+    const target = event.target;
+    const hole = target.closest('.hole');
+    if (!event.isTrusted || !hole || !hole.classList.contains('up')) return;
+    score++;
+    scoreBoard.textContent = score.toString();
+    hole.classList.remove('up');
+}
+holes.forEach(hole => {
+    const mole = document.createElement('div');
+    mole.classList.add('mole');
+    hole.appendChild(mole);
+    hole.addEventListener('click', whack);
+});
+//
 function select_board() {
     if(!select_tama) return console.log(`empty tama List`)
     Tamas.forEach((new_tama) => {
@@ -113,7 +196,12 @@ function select_board() {
             if(!start) return console.log(`empty start div`)
             start.classList.toggle(`hidden`)
             Selected_Tama = new_tama
-            update_bars(RNG(60,80),RNG(40,70),RNG(40,70))
+            tama_hp = RNG(60,80)
+            tama_hunger = RNG(40,70)
+            tama_fun = RNG(40,70)
+            tama_exp = 0
+            tama_lvl = 0
+            update_bars(tama_hp,tama_hunger,tama_fun,tama_exp)
             add_tama_to_playground(Selected_Tama)
         }
         select_tama.appendChild(tama);
@@ -123,26 +211,70 @@ function add_tama_to_playground(new_tama: Tama_emoji) {
     if(play_area){
         const newTama = document.createElement('div');
         newTama.classList.add('new_tama');
+        newTama.style.fontSize = `${size}px`
         newTama.textContent = new_tama.emoji;
         play_area.appendChild(newTama);
-        move_anim(newTama,play_area.clientWidth - newTama.clientWidth, play_area.clientHeight - newTama.clientHeight)
+        move_anim(newTama)
     }
 }
-function move_anim(newTama:HTMLDivElement, maxX: number, maxY:number, ){
-    const duration = RNG(2000, 5000)
-    const delay = RNG(0, 1000)
-    newTama.style.transition = `left ${duration}ms ease-out ${delay}ms, top ${duration}ms ease-out ${delay}ms`;
-    newTama.style.left = `${RNG(0,maxX)}px`;
-    newTama.style.top = `${RNG(0,maxY)}px`;
+function move_anim(newTama:HTMLDivElement){
+    let maxX: number
+    let maxY: number
+    if(play_area){
+        maxX = play_area.clientWidth - newTama.clientWidth
+        maxY = play_area.clientHeight - newTama.clientHeight
 
-    setTimeout(() => {
-        move_anim(newTama,maxX, maxY)
-    }, duration + delay);
+        const duration = RNG(2000, 5000)
+        const delay = RNG(0, 1000)
+        newTama.style.transition = `left ${duration}ms ease-out ${delay}ms, top ${duration}ms ease-out ${delay}ms`;
+        newTama.style.left = `${RNG(0,maxX)}px`;
+        newTama.style.top = `${RNG(0,maxY)}px`;
+
+        setTimeout(() => {
+            if(move){
+                check_status(newTama)
+                update_bars(tama_hp, tama_hunger,tama_fun,tama_exp)
+                move_anim(newTama)
+            }
+        }, duration + delay);
+    }
 }
-function update_bars(hp: number, hunger: number, fun: number) {
+function check_status(newTama:HTMLDivElement){
+    if(tama_hp > 0){
+        if(tama_fun > 0 && tama_hunger > 0){
+            if(tama_lvl < 100){
+                tama_hunger--
+                tama_fun--
+                tama_exp +=  (100 - tama_lvl) / 100
+                if(tama_exp >= 100){
+                    tama_exp = 0
+                    lvl_up(newTama)
+                }
+            }
+        }
+        else{
+            tama_hp -=  (100 - tama_lvl) / 100
+        }
+    }
+   else {
+       move = false
+       newTama.remove()
+        end_wrapper?.classList.toggle(`hidden`)
+    }
+}
+function lvl_up(newTama : HTMLDivElement){
+    tama_lvl++;
+    size += 5
+    newTama.style.fontSize = `${size}px`
+    if(lvl_display)
+        lvl_display.textContent = tama_lvl.toString()
+
+}
+function update_bars(hp: number, hunger: number, fun: number, lvl: number) {
     if (hp_bar) setProgress(hp_bar, hp);
     if (hunger_bar) setProgress(hunger_bar, hunger);
     if (fun_bar) setProgress(fun_bar, fun);
+    if (lvl_bar) setProgress(lvl_bar, lvl);
 }
 function setProgress(element: HTMLDivElement, value: number) {
     element.style.width = value + '%';
